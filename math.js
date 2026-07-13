3c7cd6e3fb9a740b69a2f41ed98349ee3b273750
ReinaldoSM <reinaldodasilvamoraes772@gmail.com>
2026-07-13 14:40:13 -0300

    First Commit

---
+++ math.js
@@ -0,0 +1,29 @@
+/*Math -> Matematica
+
+    - pow(2,2) / potência / 2² = 4
+    - sqrt(25,2) / raiz / √25 -> que numero vezes ele emsmo da 25
+    - PI / 3,14
+    - ceil / teto -> arredondar pra cima
+    - floor / chão arredondar pra baixo
+
+    - random() / número aleatório entre 0 e 1
+*/
+
+// const result = Math.pow(2,2);
+// console.log(result);
+
+// const result = Math.sqrt(25,2)
+// console.log(result)
+
+// const result = Math.PI;
+// console.log(result);
+
+// const result = Math.floor(3.14);
+// console.log(result);
+
+// const result = Math.random();
+// console.log(result);
+
+
+
+
